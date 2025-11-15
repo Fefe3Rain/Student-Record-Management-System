c3c111fb41cd6a4c82ac8c65b7fa33cad39776a3
Fefe3Rain <122379830+Fefe3Rain@users.noreply.github.com>
2025-11-15 17:28:37 +0800

    Basic page

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,29 @@
 package application;
 
-public class Main {
-    
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;  
+import javafx.scene.paint.Color;
+import javafx.scene.image.Image;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.LIGHTGRAY);
+
+        Image icon = new Image("file:///C:/Users/Rain Sidney/OneDrive/Documents/BSIT College Files/2nd Year 1st Sem/OOP/Student Record Management System/Images/TUP Logo.png");
+        stage.getIcons().add(icon);
+
+        stage.setTitle("TUP-M SRMS");
+        stage.setResizable(false);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
